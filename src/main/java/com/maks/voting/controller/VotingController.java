3f6738fe464ccbff5bffe6e55c01d79a8a12bb12
Maksimys87 <maksimys87051@gmail.com>
2018-12-16 13:10:14 +0300

    Added logging

--- src/main/java/com/maks/voting/controller/VotingController.java
+++ src/main/java/com/maks/voting/controller/VotingController.java
@@ -17,6 +17,7 @@ import java.util.List;
 @RestController
 @RequestMapping("voting")
 public class VotingController {
+    private final Logger logger = LoggerFactory.getLogger(getClass());
 
     private final ThemeService themeService;
 
@@ -33,14 +34,18 @@ public class VotingController {
 
     @PostMapping("create")
     public Theme createTheme(@RequestBody Theme theme) {
+        logger.info("create {}", theme);
         return themeService.create(theme);
     }
 
 
     @PutMapping("start/{themeId}")
     public Reference startVoting(@PathVariable("themeId") Theme theme) {
+        logger.info("start method, get {}", theme);
         if (theme.isEnabled())
             return referenceService.getByThemeId(theme.getId());
+
+        logger.info("start {}", theme);
 
         Theme updateTheme = themeService.updateEnabled(theme, true);
 
@@ -53,8 +58,11 @@ public class VotingController {
 
     @PutMapping("stop/{themeId}")
     public Theme stopVoting(@PathVariable("themeId") Theme theme) {
+        logger.info("stop method, get {}", theme);
         if (!theme.isEnabled())
             return theme;
+
+        logger.info("stop{}", theme);
 
         Theme updateTheme = themeService.updateEnabled(theme, false);
 
@@ -66,26 +74,31 @@ public class VotingController {
 
     @GetMapping("theme/{themeId}/voices/{itemId}")
     public Item getItem(@PathVariable("themeId") Theme theme, @PathVariable("itemId") Item item) {
+        logger.info("get {} of {}", item, theme);
         return itemService.getItemOfTheme(theme, item);
     }
 
     @GetMapping("theme/{themeId}")
     public Theme getTheme(@PathVariable("themeId") Theme theme) {
+        logger.info("get {}", theme);
         return themeService.get(theme);
     }
 
     @PutMapping("theme/{themeId}/item/{itemId}")
     public Item voteRegistration(@PathVariable("themeId") Theme theme, @PathVariable("itemId") Item item) {
+        logger.info("vote registration for {} of {} ", item, theme);
         return itemService.voiceRegistration(theme, item);
     }
 
     @GetMapping                                                            //optional
     public List<Theme> getAllThemes() {
+        logger.info("getAllThemes");
         return themeService.getAll();
     }
 
     @DeleteMapping("theme/{themeId}/delete")                               //optional
     public Theme deleteTheme(@PathVariable("themeId") Theme theme) {
+        logger.info("delete {}", theme);
         themeService.delete(theme);
         return theme;
     }
